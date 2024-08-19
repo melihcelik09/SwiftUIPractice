e752a8abe3588cae3a08f11c6cf9d67dd96b9641
Melih Çelik <melihcelik@izmir.bel.tr>
2024-08-19 14:07:20 +0300

    Initial Commit

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Melih on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
